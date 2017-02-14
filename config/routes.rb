12256dd8007455ba5a8b5660c16ba7549f486cd8
Alex <alex@webscalenetworks.com>
2017-02-14 10:53:27 -0700

    Added rspec and completed api/v1 routes with testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
-Rails.application.routes.draw do
-    namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
-      scope module: :v1 do
+require 'api_constraints'
 
-      end
+Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+
     end
+  end
 end
